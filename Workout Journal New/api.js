c41d5ed97158fde3a69048b4374c0d744905ef63
Eric Mathusek <eamathusek@gmail.com>
2019-07-11 11:49:34 -0600

    testing

--- Workout Journal New/api.js
+++ Workout Journal New/api.js
@@ -4,7 +4,25 @@ async function fetchApi(url){
     const response = await fetch(baseurl + url);
     const data = await response.json();
     console.log(data);
+    return data;
 }
-fetchApi("exercise");
+// fetchApi("exercise");
+
+async function listWorkout(){
+  const workouts = await fetchApi("exercise");
+  const elementList = document.getElementById("exercise");
+  workouts.results.forEach(element => {
+    elementList.appendChild(renderExercise(element))
+  });
+}
+
+function renderExercise(exercise){
+  const item = document.createElement("li");
+  item.innerHTML = `name:${exercise.name}`;
+  //item.addEventListener('click');
+  return item;
+}
+
+listWorkout();
 
 
