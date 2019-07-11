d06215dd6798dacbabc83b146ec3e832260379a5
Eric Mathusek <eamathusek@gmail.com>
2019-07-11 09:15:43 -0600

    testing

--- Workout Journal New/api.js
+++ Workout Journal New/api.js
@@ -7,18 +7,4 @@ async function fetchApi(url){
 }
 fetchApi("exercise");
 
-unirest.get("https://food-calorie-data-search.p.rapidapi.com/api/search?keyword=apple")
-.header("X-RapidAPI-Host", "food-calorie-data-search.p.rapidapi.com")
-.header("X-RapidAPI-Key", "af42b18fbdmsh5cea9dcbd511d68p1add7fjsn750c5b4f114a")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
 
-const baseurl = "https://food-calorie-data-search.p.rapidapi.com/api/search?keyword=apple";
-
-async function fetchApi(url){
-    const response = await fetch(baseurl + url);
-    const data = await response.json();
-    console.log(data);
-}
-fetchApi("calories");
